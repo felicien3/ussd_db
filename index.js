9e67db9ffe1e68f7c1af2e7b7548179790a391b4
Felicien Niyonsenga <niyonsengafelicien11@gmail.com>
2025-05-20 12:59:18 +0200

    added file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const app = require('./src/server');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ussd = require('./src/ussd');
+const port = process.env.PORT || 3000;
+
+// Make sure database and tables are initialized
+require('./src/db');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/ussd', ussd);
+
+app.listen(port, () => console.log(`USSD app listening on port ${port}!`));
+
+module.exports = app;
